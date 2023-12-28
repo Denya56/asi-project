78b6bb991795e40a5ac417140267e9adef623edb
s22264 <s22264@pjwstk.edu.pl>
2023-12-28 16:43:01 +0100

    Finished the PyCaret.

--- kedro/my-project/src/my_project/pipelines/pycaret_pipeline/pycaret_pipeline.py
+++ kedro/my-project/src/my_project/pipelines/pycaret_pipeline/pycaret_pipeline.py
@@ -1,23 +1,29 @@
 # src/my_project/pipelines/pycaret_pipeline
 
 from kedro.pipeline import Pipeline, node
-from .nodes import preprocess_data, train_pycaret_automl
+from .nodes import preprocess_and_train_pycaret_automl, predict_with_best_model, compute_scoring
 
 def create_pycaret_pipeline(**kwargs):
     return Pipeline(
         [
             node(
-                preprocess_data,
+                preprocess_and_train_pycaret_automl,
                 ["raw_data", "parameters"],
-                ["preprocessed_data"],
-                name="preprocess_data_node",
+                {"model_artifacts": "model_artifacts"},  # Update the output format
+                name="preprocess_and_train_pycaret_automl_node",
             ),
             node(
-                train_pycaret_automl,
-                ["preprocessed_data", "parameters"],
-                ["model", "model_artifacts"],
-                name="train_pycaret_automl_node",
+                predict_with_best_model,
+                ["raw_data", "model_artifacts"],  # Update to use "model_artifacts"
+                "predictions",
+                name="predict_with_best_model_node",
             ),
+            node(
+    		compute_scoring,
+    		["predictions", "model_artifacts"],  # Update to use "model_artifacts"
+    		"model_score",
+	        name="compute_scoring_node",
+	    ),
         ],
         tags=["pycaret"],
     )
