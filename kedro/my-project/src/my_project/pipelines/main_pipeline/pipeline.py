bc667091fbeb4d9161b903c9c5043afa15a0e91e
s22264 <s22264@pjwstk.edu.pl>
2023-12-29 16:13:34 +0100

    Optuna and PyCAret are both finished. Added new nodes to the pipeline and fixed bugs.

--- kedro/my-project/src/my_project/pipelines/main_pipeline/pipeline.py
+++ kedro/my-project/src/my_project/pipelines/main_pipeline/pipeline.py
@@ -7,7 +7,7 @@ from kedro.pipeline import Pipeline, pipeline
 from kedro.pipeline.node import node
 
 from my_project.pipelines.shared_nodes import load_nlp, tokenizer, prepare_data
-from .nodes import hi, load_train_data, train_model
+from .nodes import hi, load_train_data, train_model, find_hyperparameters
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -17,6 +17,7 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(tokenizer, inputs=None, outputs="tokenizer", name="tokenizer"),
             node(load_train_data, inputs=None, outputs="df", name="df"),
             node(prepare_data, inputs={"df": "df", "nlp": "nlp", "tokenizer": "tokenizer"}, outputs=["labels", "reviews"], name="data_prepared"),
-            node(train_model, inputs={"reviews": "reviews", "labels":"labels"}, outputs=None, name="model")
+            node(train_model, inputs={"reviews": "reviews", "labels":"labels"},  outputs="trained_model", name="model"),
+	    node(find_hyperparameters, inputs={"reviews": "reviews", "labels": "labels", "pretrained_model": "trained_model"}, outputs=None, name="find_hyperparameters")
         ]
     )
