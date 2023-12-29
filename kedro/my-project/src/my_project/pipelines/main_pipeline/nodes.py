fa8483138e3ff4406d8e6b691cfbd3bb6f2f557f
s22264 <s22264@pjwstk.edu.pl>
2023-12-29 16:13:34 +0100

    Optuna and PyCAret are both finished. Added new nodes to the pipeline and fixed bugs.

--- kedro/my-project/src/my_project/pipelines/main_pipeline/nodes.py
+++ kedro/my-project/src/my_project/pipelines/main_pipeline/nodes.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import tensorflow as tf
 import keras
+import optuna
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras import layers
 from my_project.pipelines.shared_nodes import names, max_len, max_words
 from datetime import datetime
-
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from optuna.integration import KerasPruningCallback
 import pickle
 # nodes.py
-
     
 def hi():
     return "xdd"
     
 def load_train_data():
-    df = pd.read_csv('~/ASI_2/asi-project/data/train-data/train_dataset.csv', names=names)
+    df = pd.read_csv('data/01_raw/train_dataset.csv-20231228T102241Z-001/train_dataset.csv', names=names)
     return df
 
 def train_model(reviews, labels):
@@ -40,5 +43,39 @@ def train_model(reviews, labels):
         pickle.dump(model3, file)
     #model3.save('~/ASI_2/asi-project/kedro/my-project/data/06_models/model.h5')
     
+    return model3
+def find_hyperparameters(reviews, labels, pretrained_model):
+    # Define the objective function for Optuna
+    def objective(trial):
+        # Define the hyperparameters to optimize
+        max_words = trial.suggest_int('max_words', 100, 1000)
+        dropout_rate = trial.suggest_uniform('dropout_rate', 0.2, 0.8)
+        lstm_units = trial.suggest_int('lstm_units', 10, 50)
 
+        # Load the pretrained model
+        model = pretrained_model
+
+        
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        # epochs changed to 1 from 10 (remember to change back)
+        history = model.fit(reviews, labels, epochs=1, validation_split=0.1, verbose=0, callbacks=[KerasPruningCallback(trial, 'accuracy')])
+
+        # Evaluate the model on the validation set
+        accuracy = history.history['accuracy'][-1]
+
+        return accuracy
+
+    # Create an Optuna study
+    study = optuna.create_study(direction='maximize', pruner=optuna.pruners.MedianPruner())
+    study.optimize(lambda trial: objective(trial), n_trials=100)
+
+    # Get the best hyperparameters
+    best_params = study.best_params
+    print(f"Best Hyperparameters: {best_params}")
+
+    # You can use these hyperparameters as needed
+    # ...
+
+    return best_params
     
